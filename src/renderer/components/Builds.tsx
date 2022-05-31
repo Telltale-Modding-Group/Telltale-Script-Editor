29aee24835d8a1ca36b656b3cec340560446a675
bigbeno37 <contact@benosullivan.com>
2022-05-31 18:21:47 +1000

    Build archives can now be cleared from the sidebar

--- src/renderer/components/Builds.tsx
+++ src/renderer/components/Builds.tsx
@@ -1,32 +1,71 @@
 import styles from './Sidebar.module.css';
-import {Button, Code, Group, MediaQuery, Stack} from '@mantine/core';
+import {ActionIcon, Button, Code, Group, MediaQuery, Stack} from '@mantine/core';
 import * as React from 'react';
 import {useAppDispatch, useAppSelector} from '../slices/store';
 import {BuildsActions} from '../slices/BuildsSlice';
 import {MainProcess} from '../MainProcessUtils';
+import {BsFillTrashFill} from 'react-icons/bs';
+import {showNotification} from '@mantine/notifications';
+import {FileTreeActions, FileTreeAsyncActions} from '../slices/FileTreeSlice';
 
 export const Builds = () => {
 	const dispatch = useAppDispatch();
 	const logs = useAppSelector(state => state.builds.logs);
+	const root = useAppSelector(state => state.filetree.root);
 
-	const handleClearLogs = () => dispatch(BuildsActions.clear());
+	if (!root?.directory) return null;
+
+	const handleClearLogs = () => {
+		dispatch(BuildsActions.clear());
+		showNotification({
+			title: 'Build logs cleared',
+			message: 'Logs associated with the latest build were cleared!',
+			color: 'green'
+		});
+	}
 
 	const handleOpenBuildsDirectory = () => {
 		MainProcess.openBuildsDirectory();
 	};
 
+	const handleClearBuildArchives = async () => {
+		const buildsDirectory = root.children.find(file => file.name === 'Builds');
+
+		if (!buildsDirectory) return;
+
+		await MainProcess.deleteFile(buildsDirectory);
+		dispatch(FileTreeAsyncActions.refreshRootDirectory());
+
+		showNotification({
+			title: 'Cleared build archives',
+			message: 'All build archives were deleted.',
+			color: 'green'
+		});
+	};
+
 	return <div className={styles.stackContainer}>
 		<Stack className={styles.stack}>
+			{logs.length > 0 &&
+				<div style={{ position: 'absolute', alignSelf: 'end' }}>
+					<ActionIcon
+						color="red"
+						variant="light"
+						onClick={handleClearLogs}
+						style={{ position: 'relative', top: '1rem', right: '2rem' }}
+					>
+						<BsFillTrashFill size={14}/>
+					</ActionIcon>
+				</div>
+			}
+
 			<Code block className={styles.logs}>
 				{logs.join('\n')}
 			</Code>
 
-			<MediaQuery query="min-width: 501px" styles={{}}>
-				<Group position="right" p="xs">
-					<Button onClick={handleOpenBuildsDirectory}>Open Builds Directory</Button>
-					<Button onClick={handleClearLogs} color="red">Clear</Button>
-				</Group>
-			</MediaQuery>
+			<Group position="right" p="xs">
+				<Button onClick={handleClearBuildArchives} color="red">Clear Build Archives</Button>
+				<Button onClick={handleOpenBuildsDirectory}>Open Builds Directory</Button>
+			</Group>
 		</Stack>
 	</div>
 };

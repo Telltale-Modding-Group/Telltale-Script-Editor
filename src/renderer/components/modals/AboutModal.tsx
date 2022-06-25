58c7f33880354deea07873aa39328ea1ec1f9b06
bigbeno37 <contact@benosullivan.com>
2022-06-25 10:40:56 +1000

    Updated version number

--- src/renderer/components/modals/AboutModal.tsx
+++ src/renderer/components/modals/AboutModal.tsx
@@ -5,7 +5,7 @@ export const AboutModal = () => {
 	return <Container>
 		<Stack align="center">
 			<Title order={1}>Telltale Script Editor</Title>
-			<Text>Version 2.1.1</Text>
+			<Text>Version 2.2.0</Text>
 		</Stack>
 	</Container>
 };
